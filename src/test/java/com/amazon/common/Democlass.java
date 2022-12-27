ea00e0244183478a48a37cb724e1d16ba2ae2aa8
Reshmachachar <reshma.chachar@cogniwize.com>
2022-12-27 12:36:17 +0530

    By manjiri on testing

---
+++ src/test/java/com/amazon/common/Democlass.java
@@ -0,0 +1,10 @@
+package com.amazon.common;
+
+public class Democlass {
+
+	public static void main(String[] args) {
+		System.out.println("On develop branch");
+		System.out.println("On testing branch");
+	}
+
+}
